5521b8e9538e4a4f8826530a088710c96345435c
Andrew Peng <andrewpeng02@gmail.com>
2023-12-21 11:02:35 -0600

    working aci:push

---
+++ mleng-transformer-translation/example_deploy_script.py
@@ -0,0 +1,21 @@
+from prefect import flow
+from prefect.deployments import DeploymentImage
+
+import torch
+
+@flow(log_prints=True)            
+def my_flow(name: str = "world"):           
+    print(torch.cuda.is_available())               
+    print(f"Hello {name}! I'm a flow running on an Azure Container Instance!")
+
+
+if __name__ == "__main__":
+    my_flow.deploy(
+        name="my-deployment", 
+        work_pool_name="my-aci-pool",
+        image=DeploymentImage(                                                 
+            name="prefect-flows:latest",
+            platform="linux/amd64",
+            dockerfile="Dockerfile"
+        )                                                                      
+    )       

53c6a27ae04d204eff84cb60ae9f521adfc6efe1
Andrew Peng <andrewpeng02@gmail.com>
2023-12-28 15:42:34 -0600

    small fix

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,4 @@
-import { useCallback, useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { useDebounce } from "@uidotdev/usehooks";
 import ReactTextareaAutosize from "react-textarea-autosize";
 import {
